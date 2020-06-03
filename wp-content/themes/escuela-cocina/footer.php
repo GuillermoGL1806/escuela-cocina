a6727f1f411666c35a07ff8a282eaf10f9fc5b20
Marco Aspeitia <maspeitia.31@gmail.com>
2020-06-03 11:42:33 -0500

    Updating header.php, creating footer.php and call them in index.php, class 39

---
+++ wp-content/themes/escuela-cocina/footer.php
@@ -0,0 +1,29 @@
+<footer class="footer p-5">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-6">
+            <nav
+              class="nav text-uppercase d-flex flex-column flex-md-row text-center text-md-left"
+            >
+              <a href="nosotros.html" class="nav-link">Nosotros</a>
+              <a href="blog.html" class="nav-link">Blog</a>
+              <a href="clases.html" class="nav-link">Clases</a>
+              <a href="galeria.html" class="nav-link">Galería</a>
+              <a href="contacto.html" class="nav-link">Contacto</a>
+            </nav>
+          </div>
+          <!-- col-md-6 -->
+          <div class="col-md-6">
+            <p class="text-center text-md-right mt-4 mt-md-0 copyright">
+              Todos los derechos reservados
+            </p>
+          </div>
+          <!-- col-md-6 -->
+        </div>
+        <!-- row -->
+      </div>
+      <!-- container -->
+    </footer>
+    <?php wp_footer(); ?>
+  </body>
+</html>
--- wp-content/themes/escuela-cocina/header.php
+++ wp-content/themes/escuela-cocina/header.php
@@ -10,3 +10,46 @@
   </head>
 
   <body>
+  <header class="header py-5">
+      <!-- Top Navbar -->
+      <div class="container">
+        <!-- Init display flex with class row -->
+        <div class="row justify-content-center align-items-center">
+          <div id="logo" class="col-md-4 col-8 mb-4 mb-md-0">
+            <a class="navbar-brand" href="index.html">
+              <img src="img/logo.svg" class="img-fluid" />
+            </a>
+          </div>
+          <!-- logo -->
+          <div id="menu" class="col-md-8">
+            <nav
+              class="navbar navbar-expand-md navbar-light justify-content-center"
+            >
+              <button
+                class="navbar-toggler mb-4"
+                data-toggle="collapse"
+                data-target="#mainNavbar"
+              >
+                <!-- responsive menu -->
+                <span class="navbar-toggler-icon"></span>
+              </button>
+              <div
+                id="mainNavbar"
+                class="collapse navbar-collapse justify-content-center justify-content-lg-end text-center text-uppercase"
+              >
+                <a href="nosotros.html" class="nav-link">Nosotros</a>
+                <a href="blog.html" class="nav-link active">Blog</a>
+                <a href="clases.html" class="nav-link">Clases</a>
+                <a href="galeria.html" class="nav-link">Galería</a>
+                <a href="contacto.html" class="nav-link">Contacto</a>
+              </div>
+              <!-- mainNavbar -->
+            </nav>
+            <!-- navbar -->
+          </div>
+          <!-- menu -->
+        </div>
+        <!-- row -->
+      </div>
+      <!-- container -->
+    </header>
