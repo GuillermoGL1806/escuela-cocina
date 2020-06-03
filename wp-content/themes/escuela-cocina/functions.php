f475cf419812c77c3a1fe2d23f5e034c58f0eb3f
Marco Aspeitia <maspeitia.31@gmail.com>
2020-06-03 12:14:17 -0500

    Creating nav menu in functions.php, class 40

--- wp-content/themes/escuela-cocina/functions.php
+++ wp-content/themes/escuela-cocina/functions.php
@@ -3,6 +3,17 @@
 *   Load theme's CSS and Scritps
 */
 
+
+/* Functions that loads when the theme is activated */
+function edc_setup(){
+    /* Nav Menus */
+    register_nav_menus( array(
+        'main_menu' => esc_html__('Menu Principal', 'escuelacocina') /* escuelacocina is located in styles.css Text Domain */
+    ) );
+}
+add_action( 'after_setup_theme', 'edc_setup' );
+
+
 function edc_scripts(){
     /* Styles */
     wp_enqueue_style('bootstrap-css', get_template_directory_uri() . '/css/bootstrap.min.css', false, '4.5.0');
