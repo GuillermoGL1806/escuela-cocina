8b0b9533e72417f56af95100d765ed9c5d07b405
Marco Aspeitia <maspeitia.31@gmail.com>
2020-06-03 11:18:37 -0500

    Adding stylesheets to functions.php, class 38

--- wp-content/themes/escuela-cocina/functions.php
+++ wp-content/themes/escuela-cocina/functions.php
@@ -5,5 +5,7 @@
 
 function edc_scripts(){
     wp_enqueue_style('bootstrap-css', get_template_directory_uri() . '/css/bootstrap.min.css', false, '4.5.0');
+
+    wp_enqueue_style('style', get_stylesheet_uri(), array('bootstrap-css') );
 }
 add_action( 'wp_enqueue_scripts', 'edc_scripts' );
