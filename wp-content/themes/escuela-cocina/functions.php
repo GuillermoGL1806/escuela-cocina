3f8ccd5ff2afa9db47c798c87853121d50ef4e8d
Marco Aspeitia <maspeitia.31@gmail.com>
2020-06-03 11:37:10 -0500

    Adding scripts to functions.php, class 39

--- wp-content/themes/escuela-cocina/functions.php
+++ wp-content/themes/escuela-cocina/functions.php
@@ -4,8 +4,15 @@
 */
 
 function edc_scripts(){
+    /* Styles */
     wp_enqueue_style('bootstrap-css', get_template_directory_uri() . '/css/bootstrap.min.css', false, '4.5.0');
 
     wp_enqueue_style('style', get_stylesheet_uri(), array('bootstrap-css') );
+
+    /* Scripts */
+    wp_enqueue_script('jquery');
+    wp_enqueue_script('popper', get_template_directory_uri() . '/js/popper.min.js', array('jquery'), '2.4.0', true);
+    wp_enqueue_script('bootstrap', get_template_directory_uri() . '/js/bootstrap.min.js', array('jquery'), '4.5.0', true)
+
 }
 add_action( 'wp_enqueue_scripts', 'edc_scripts' );
