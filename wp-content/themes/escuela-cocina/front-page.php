297c928a0a2ac7cf3027ccc4ef4a8a7233e4d3d1
Marco Aspeitia <maspeitia.31@gmail.com>
2020-06-04 01:35:04 -0500

    Contact permalink indexed in school-subject btn more information, class 52

--- wp-content/themes/escuela-cocina/front-page.php
+++ wp-content/themes/escuela-cocina/front-page.php
@@ -1,12 +1,14 @@
 <?php get_header(); 
   if (have_posts() ) :
     while (have_posts()) : the_post();
+    /* printf ('<pre>%s</pre>', var_export(get_post_custom( get_the_ID()), true) ); debugging*/
     $textHero1 = get_post_meta( get_the_ID(), 'homepage_top_text_1', true);
     $textHero2 = get_post_meta( get_the_ID(), 'homepage_top_text_2', true);
     $imageHero1 = get_post_meta( get_the_ID(), 'homepage_image_1', true);
     $imageHero2 = get_post_meta( get_the_ID(), 'homepage_image_2', true);
     $bottomText = get_post_meta( get_the_ID(), 'homepage_bottom_text', true);
     $bottomImage = get_post_meta( get_the_ID(), 'homepage_bottom_image', true);
+    $contacto = get_page_by_title('Contacto');
 ?>
     <div class="container-fluid main-images">
       <div class="row top-image image">
@@ -14,7 +16,7 @@
           <div class="row justify-content-center align-items-center h-100">
             <div class="col-sm-8 col-md-6">
               <div class="content text-center text-light py-3">
-                <?php echo $textHero1 ?>
+                <?php echo $textHero1; ?>
               </div>
               <!-- content -->
             </div>
@@ -23,7 +25,7 @@
           <!-- content row -->
         </div>
         <!-- col-md-6 -->
-        <div class="col-md-6 images-hero" style="background-image: url(<?php echo $imageHero1 ?>)">
+        <div class="col-md-6 images-hero" style="background-image: url(<?php echo $imageHero1; ?>)">
         </div>
       </div>
       <!-- main top row -->
@@ -32,7 +34,7 @@
           <div class="row justify-content-center align-items-center h-100">
             <div class="col-sm-8 col-md-6">
               <div class="content text-center py-3">
-                <?php echo $textHero2?>
+                <?php echo $textHero2; ?>
               </div>
               <!-- content -->
             </div>
@@ -41,7 +43,7 @@
           <!-- content row -->
         </div>
         <!-- col-md-6 -->
-        <div class="col-md-6 images-hero" style="background-image: url(<?php echo $imageHero2 ?>)"></div>
+        <div class="col-md-6 images-hero" style="background-image: url(<?php echo $imageHero2; ?>)"></div>
       </div>
       <!-- main bottom row -->
     </div>
@@ -278,8 +280,8 @@
         <div class="row justify-content-center align-items-center">
           <div class="col-md-8">
             <div class="content text-center text-light">
-              <?php echo $bottomText ?>
-              <a href="contacto.html" class="btn btn-primary text-light">Más información</a>
+              <?php echo $bottomText; ?>
+              <a href="<?php echo get_permalink($contacto->ID)?>" class="btn btn-primary text-light">Más información</a>
             </div>
           </div>
         </div>
