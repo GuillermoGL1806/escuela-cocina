c22f8347a842346ae729213c45b6a2f650a3fd7c
Marco Aspeitia <maspeitia.31@gmail.com>
2020-06-06 08:39:03 -0500

    CReating template-part icons.php

--- wp-content/themes/escuela-cocina/page-icons.php
+++ wp-content/themes/escuela-cocina/page-icons.php
@@ -8,40 +8,7 @@ get_header();
 while (have_posts()) : the_post();
 
 get_template_part('template-parts/content', 'pages');
-?>
 
-<section class="us mt-5 container">
-    <h2 class="text-center mb-5 divider">
-        ¿Por qué estudiar con nosotros?
-    </h2>
-    <div class="row">
-        <div class="col-md-4 text-center information">
-            <img src="img/icono_chef.png" alt="icono chef" class="img-fluid mb-3" />
-            <h3>Chef's especialistas</h3>
-            <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            </p>
-        </div>
-        <!-- First col-md-4 -->
-        <div class="col-md-4 text-center information">
-            <img src="img/icono_vino.png" alt="icono chef" class="img-fluid mb-3" />
-            <h3>Incluye todo sobre bebidas</h3>
-            <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            </p>
-        </div>
-        <!-- Second col-md-4 -->
-        <div class="col-md-4 text-center information">
-            <img src="img/icono_menu.png" alt="icono chef" class="img-fluid mb-3" />
-            <h3>Siempre actualizado</h3>
-            <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            </p>
-        </div>
-        <!-- Third col-md-4 -->
-    </div>
-</section>
 
-<?php
 endwhile;
 get_footer();
