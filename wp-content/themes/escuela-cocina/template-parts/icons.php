0ca4f25ed78d0e1f90f602ad7fbbd80c16bd426a
Marco Aspeitia <maspeitia.31@gmail.com>
2020-06-06 11:25:32 -0500

    Finishing group icon metaboxes in icons.php, class 63

--- wp-content/themes/escuela-cocina/template-parts/icons.php
+++ wp-content/themes/escuela-cocina/template-parts/icons.php
@@ -1,42 +1,24 @@
 <?php printf ('<pre>%s</pre>', var_export(get_post_custom( get_the_ID()), true) );
     $title = get_post_meta( get_the_ID() , 'edc_group_icon_title', true);/* bring the metabox title */
-    $iconInformation = get_post_meta( get_the_ID() , 'edc_group_icons', true);/* bring the metabox title */
+    $iconsInformation = get_post_meta( get_the_ID() , 'edc_group_icons', true);/* bring the metabox group */
 ?>
 
 <section class="icon mt-5 container">
     <h2 class="text-center mb-5 divider">
-        <?php echo $title ?>
+        <?php echo $title; ?>
     </h2>
-    <div class="row">
+    <div class="row justify-content-center">
         <?php
-        echo "<pre>";
-        var_dump($iconInformation);
-        echo "<pre>";
-        
+        foreach($iconsInformation as $iconInformation){
         ?>
         <div class="col-md-4 text-center information">
-            <img src="img/icono_chef.png" alt="icono chef" class="img-fluid mb-3" />
-            <h3>Chef's especialistas</h3>
+            <img src="<?php echo $iconInformation['image_icon']; ?>" alt="<?php echo $iconInformation['title_icon']; ?>"
+                class="img-fluid mb-3" />
+            <h3><?php echo $iconInformation['title_icon']; ?></h3>
             <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                <?php echo $iconInformation['description_icon']; ?>
             </p>
         </div>
-        <!-- First col-md-4 -->
-        <div class="col-md-4 text-center information">
-            <img src="img/icono_vino.png" alt="icono chef" class="img-fluid mb-3" />
-            <h3>Incluye todo sobre bebidas</h3>
-            <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            </p>
-        </div>
-        <!-- Second col-md-4 -->
-        <div class="col-md-4 text-center information">
-            <img src="img/icono_menu.png" alt="icono chef" class="img-fluid mb-3" />
-            <h3>Siempre actualizado</h3>
-            <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            </p>
-        </div>
-        <!-- Third col-md-4 -->
+        <?php } ?>
     </div>
 </section>
