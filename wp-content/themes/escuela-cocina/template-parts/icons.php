23303e5568220779221ec90a57a56ae47ea08347
Marco Aspeitia <maspeitia.31@gmail.com>
2020-06-06 11:10:18 -0500

    starting dinamic icon section, class 62

--- wp-content/themes/escuela-cocina/template-parts/icons.php
+++ wp-content/themes/escuela-cocina/template-parts/icons.php
@@ -1,13 +1,19 @@
 <?php printf ('<pre>%s</pre>', var_export(get_post_custom( get_the_ID()), true) );
     $title = get_post_meta( get_the_ID() , 'edc_group_icon_title', true);/* bring the metabox title */
-    
+    $iconInformation = get_post_meta( get_the_ID() , 'edc_group_icons', true);/* bring the metabox title */
 ?>
 
-<section class="us mt-5 container">
+<section class="icon mt-5 container">
     <h2 class="text-center mb-5 divider">
         <?php echo $title ?>
     </h2>
     <div class="row">
+        <?php
+        echo "<pre>";
+        var_dump($iconInformation);
+        echo "<pre>";
+        
+        ?>
         <div class="col-md-4 text-center information">
             <img src="img/icono_chef.png" alt="icono chef" class="img-fluid mb-3" />
             <h3>Chef's especialistas</h3>
