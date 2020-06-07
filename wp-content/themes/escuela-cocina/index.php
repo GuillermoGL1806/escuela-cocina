cd7ef8ef455cd275445b6df8b3c6da4619044eb8
Marco Aspeitia <maspeitia.31@gmail.com>
2020-06-07 09:42:02 -0500

    Starting blog template, and adding some blog posts, class 66

--- wp-content/themes/escuela-cocina/index.php
+++ wp-content/themes/escuela-cocina/index.php
@@ -1,3 +1,187 @@
 <?php get_header(); ?>
+<div class="container mb-4">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <blockquote class="text-center pl-3 subtitle">
+                Recetas, Recomendaciones ty Tips para Chef's Profesionales o Novatos
+            </blockquote>
+        </div>
+        <!-- col-md-8 -->
+    </div>
+    <!-- row -->
+</div>
 
+<div class="container">
+    <div class="row">
+        <main class="col-lg-9 col-md-8">
+            <h1 class="divider text-center mb-3">
+                Nuestro Blog
+            </h1>
+            <div class="row mb-4 postPreview">
+                <div class="col-md-4">
+                    <img src="img/clase1.jpg" alt="imagen de blog" class="img-fluid" />
+                </div>
+                <!-- col-md-4 image -->
+                <div class="col-md-8">
+                    <div class="postContent pt-4 pt-md-0">
+                        <a href="entrada.html">
+                            <h3>Título de la entrada</h3>
+                        </a>
+                        <div class="postMeta pt-2 pt-md-0">
+                            <p class="m-0">
+                                Escrito el: <span>20/10/18 </span>por <a href="#">Admin</a>
+                            </p>
+                        </div>
+                        <!-- metadata postContent -->
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                            Exercitationem non animi voluptas fugiat iusto consectetur
+                            voluptate sint!
+                        </p>
+                        <a href="entrada.html" class="btn btn-primary text-light">Ver entrada</a>
+                    </div>
+                    <!-- postContent -->
+                </div>
+                <!-- col-md-8 -->
+            </div>
+            <!-- row post -->
+            <div class="row mb-4 postPreview">
+                <div class="col-md-4">
+                    <img src="img/clase1.jpg" alt="imagen de blog" class="img-fluid" />
+                </div>
+                <!-- col-md-4 image -->
+                <div class="col-md-8">
+                    <div class="postContent pt-4 pt-md-0">
+                        <a href="entrada.html">
+                            <h3>Título de la entrada</h3>
+                        </a>
+                        <div class="meta pt-2 pt-md-0">
+                            <p class="m-0">
+                                Escrito el: <span>20/10/18 </span>por <a href="#">Admin</a>
+                            </p>
+                        </div>
+                        <!-- meta data postContent -->
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                            Exercitationem non animi voluptas fugiat iusto consectetur
+                            voluptate sint!
+                        </p>
+                        <a href="entrada.html" class="btn btn-primary text-light">Ver entrada</a>
+                    </div>
+                    <!-- postContent -->
+                </div>
+                <!-- col-md-8 -->
+            </div>
+            <!-- row post -->
+            <div class="row mb-4 postPreview">
+                <div class="col-md-4">
+                    <img src="img/clase1.jpg" alt="imagen de blog" class="img-fluid" />
+                </div>
+                <!-- col-md-4 image -->
+                <div class="col-md-8">
+                    <div class="postContent pt-4 pt-md-0">
+                        <a href="entrada.html">
+                            <h3>Título de la entrada</h3>
+                        </a>
+                        <div class="meta pt-2 pt-md-0">
+                            <p class="m-0">
+                                Escrito el: <span>20/10/18 </span>por <a href="#">Admin</a>
+                            </p>
+                        </div>
+                        <!-- meta data postContent -->
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                            Exercitationem non animi voluptas fugiat iusto consectetur
+                            voluptate sint!
+                        </p>
+                        <a href="entrada.html" class="btn btn-primary text-light">Ver entrada</a>
+                    </div>
+                    <!-- postContent -->
+                </div>
+                <!-- col-md-8 -->
+            </div>
+            <!-- row postPreview -->
+        </main>
+
+        <aside class="col-lg-3 col-md-4 bg-primary p-4">
+            <div class="nextCourses">
+                <h2 class="text-center text-light divider inverse">
+                    Próximos cursos
+                </h2>
+                <div class="card mb-4">
+                    <img src="img/clase1.jpg" alt="imagen de curso" class="img-fluid" />
+                    <div class="card-body">
+                        <h3 class="card-title">
+                            Comida italiana para principiantes
+                        </h3>
+                        <p class="card-subtitle m-0">
+                            Aprende las bases de la comida italiana con este curso
+                        </p>
+                    </div>
+                    <!-- card-body -->
+                    <div class="card-footer">
+                        <a href="#">Más información</a>
+                    </div>
+                    <!-- card-footer -->
+                </div>
+                <!-- main card -->
+            </div>
+            <div class="card mb-4">
+                <img src="img/clase1.jpg" alt="imagen de curso" class="img-fluid" />
+                <div class="card-body">
+                    <h3 class="card-title">
+                        Comida italiana para principiantes
+                    </h3>
+                    <p class="card-subtitle m-0">
+                        Aprende las bases de la comida italiana con este curso
+                    </p>
+                </div>
+                <!-- card-body -->
+                <div class="card-footer">
+                    <a href="#">Más información</a>
+                </div>
+                <!-- card-footer -->
+            </div>
+            <!-- main card -->
+            <div class="card mb-4">
+                <img src="img/clase1.jpg" alt="imagen de curso" class="img-fluid" />
+                <div class="card-body">
+                    <h3 class="card-title">
+                        Comida italiana para principiantes
+                    </h3>
+                    <p class="card-subtitle m-0">
+                        Aprende las bases de la comida italiana con este curso
+                    </p>
+                </div>
+                <!-- card-body -->
+                <div class="card-footer">
+                    <a href="#">Más información</a>
+                </div>
+                <!-- card-footer -->
+            </div>
+            <!-- main card -->
+            <div class="lastPosts">
+                <h2 class="text-center text-light">Últimas entradas</h2>
+                <ul class="list-unstyled mt-4 text-center">
+                    <li>
+                        <a href="#">Título de entrada</a>
+                    </li>
+                    <li>
+                        <a href="#">Título de entrada</a>
+                    </li>
+                    <li>
+                        <a href="#">Título de entrada</a>
+                    </li>
+                    <li>
+                        <a href="#">Título de entrada</a>
+                    </li>
+                </ul>
+            </div>
+            <!-- lastPosts -->
+        </aside>
+        <!-- next courses -->
+    </div>
+    <!-- main row -->
+</div>
+<!-- main container -->
 <?php get_footer(); ?>
