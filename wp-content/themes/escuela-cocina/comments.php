9639093161b9e34623f502200148427594d5bac1
Marco Aspeitia <maspeitia.31@gmail.com>
2020-06-10 20:24:09 -0500

    Adding comments.php template, class 71

---
+++ wp-content/themes/escuela-cocina/comments.php
@@ -0,0 +1,5 @@
+<div class="container ">
+    <div class="row justify-content-center">
+        <h1>Sección de comentarios</h1>
+    </div>
+</div>
--- wp-content/themes/escuela-cocina/single.php
+++ wp-content/themes/escuela-cocina/single.php
@@ -1,9 +1,18 @@
 <?php
 get_header();
-
 while (have_posts()) : the_post();
+    get_template_part('template-parts/content', 'post');
+?>
+<div class="container comments">
+    <?php
+        if(comments_open() || get_comments_number()):
+            comments_template();
+        else:
+            echo "<p class='text-center'>Comentarios deshabilitados</p>";
+        endif;
+    ?>
+</div>
 
-get_template_part('template-parts/content', 'post');
-
+<?php
 endwhile;
 get_footer();
